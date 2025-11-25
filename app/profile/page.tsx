3ef8eccbe33f17bc846620f61a8890354fc1d2ef
nguientiendat <git75749@gmail.com>
2025-11-26 00:35:29 +0700

    again

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-export const runtime = "edge";
+// export const runtime = "edge";
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 // import Link from "next/link"; // <-- Đã xóa
